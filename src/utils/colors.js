cea9213f04a7bac50d688b20f1128e109d906eb0
Fernando de Paula <di.paulaf@hotmail.com>
2020-02-14 19:11:01 -0300

    feat: animations

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,4 +1,4 @@
 export const PRIMARY_COLOR = '#9645AE';
 export const SECONDARY_COLOR = '#FCC13F';
-export const WHITE = '#FFF';
-export const BLACK = '#000';
+export const WHITE = '#FFFFFF';
+export const BLACK = '#000000';
