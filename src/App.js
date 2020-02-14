87ef09b2f23bcf17d70b9f48a528a709c71c2413
Fernando de Paula <di.paulaf@hotmail.com>
2020-02-14 18:10:18 -0300

    feat: initial project config

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>HOME</Text>
+
+      {console.log('as')}
+    </View>
+  );
+}
